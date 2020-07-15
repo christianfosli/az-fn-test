7b5944a91a7db6b35dd3283300b04e20d8b78163
Christian Fosli <cfosli@gmail.com>
2020-07-15 16:45:28 +0200

    Initial commit - MS Learn Function Apps

---
+++ WatchInfo.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class WatchInfo
+    {
+        [FunctionName("WatchInfo")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation($"{nameof(WatchInfo)} HTTP trigger function processed a request.");
+
+            if ((string)req.Query["model"] is var model && model is { })
+            {
+                // Dummy data for now
+                var watchinfo = new
+                {
+                    Manufacturer = "Orient",
+                    CaseType = "Solid",
+                    Bezel = "Titanium",
+                    Dial = "Roman",
+                    CaseFinish = "Silver",
+                    Jewels = 15,
+                };
+                return new OkObjectResult(watchinfo);
+            }
+            return new BadRequestObjectResult("Please provide a watch model in the query string");
+        }
+    }
+}
